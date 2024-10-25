2a6b11b2ad68becaf7ce8389772e06e21bbf72f7
= <jayendrawelekar03@gmail.com>
2024-10-25 09:57:59 +0530

    configured nginx to handle websocket on secured route

--- src/app/utils/SignallingManager.ts
+++ src/app/utils/SignallingManager.ts
@@ -1,7 +1,7 @@
 
 import { Ticker } from "./Types";
 
-export const BASE_URL= "wss://15.207.114.21:3001/"
+export const BASE_URL= "wss://localexchangefreedns.mooo.com/ws"
 export class SignallingManager{
     private ws: WebSocket;
     private static instance: SignallingManager;
